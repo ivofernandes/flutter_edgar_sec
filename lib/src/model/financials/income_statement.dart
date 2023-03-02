9dfa4cb939cec8294dbcead5ff3e1831e3697b36
Ivo Fernandes <ivomuze@gmail.com>
2023-03-02 12:48:19 +0000

    Added net income and operating income

--- lib/src/model/financials/income_statement.dart
+++ lib/src/model/financials/income_statement.dart
@@ -3,12 +3,16 @@ class IncomeStatement {
 
   double revenues = 0;
   double netIncome = 0;
+  double operatingIncome = 0;
 
-  double get netMargin => revenues / netIncome;
+  double get netMargin => netIncome / revenues;
+
+  double get operatingMargin => operatingIncome / revenues;
 
   @override
   String toString() => '''
   revenues: $revenues
   netIncome: $netIncome
+  operatingIncome: $operatingIncome
   ''';
 }
--- lib/src/processor/income_statement_processor.dart
+++ lib/src/processor/income_statement_processor.dart
@@ -1,5 +1,5 @@
-import 'package:flutter_edgar_sec/src/model/r3_financial_statement.dart';
 import 'package:flutter_edgar_sec/src/model/financials/income_statement.dart';
+import 'package:flutter_edgar_sec/src/model/r3_financial_statement.dart';
 import 'package:flutter_edgar_sec/src/processor/utils/base_processor.dart';
 
 /// Processes the income statement from the json response
@@ -7,10 +7,9 @@ import 'package:flutter_edgar_sec/src/processor/utils/base_processor.dart';
 ///
 /// To add a new field we need to add it to the supportedFields list and add a new case to the _mapValue method
 ///
-/// Documentation about the fields:
-/// Revenue: https://xbrl.us/forums/topic/how-to-find-a-complete-list-of-similar-concept/
 
 class IncomeStatementProcessor {
+  /// Revenue: https://xbrl.us/forums/topic/how-to-find-a-complete-list-of-similar-concept/
   static const List<String> revenueFields = [
     'RevenueFromContractWithCustomerExcludingAssessedTax',
     'SalesRevenueGoodsNet',
@@ -19,10 +18,13 @@ class IncomeStatementProcessor {
     'RevenueFromContractWithCustomerIncludingAssessedTax'
   ];
 
-  static const List<String> supportedFields = [...revenueFields];
+  static const List<String> supportedFields = [
+    ...revenueFields,
+    'NetIncomeLoss',
+    'OperatingIncomeLoss',
+  ];
 
-  static void process(
-      Map<String, dynamic> facts, Map<String, FinancialStatement> index) {
+  static void process(Map<String, dynamic> facts, Map<String, FinancialStatement> index) {
     for (final field in supportedFields) {
       // Filter the quarters, i.e. rows that are 10-Q
       final quarters = BaseProcessor.getQuarterRows(facts, field, index);
@@ -38,16 +40,18 @@ class IncomeStatementProcessor {
     }
   }
 
-  static void _mapValue(
-      String field, double value, IncomeStatement incomeStatement) {
+  static void _mapValue(String field, double value, IncomeStatement incomeStatement) {
     if (revenueFields.contains(field)) {
       incomeStatement.revenues = value;
       return;
     }
 
     switch (field) {
-      case 'RevenueFromContractWithCustomerExcludingAssessedTax':
-        incomeStatement.revenues = value;
+      case 'NetIncomeLoss':
+        incomeStatement.netIncome = value;
+        break;
+      case 'OperatingIncomeLoss':
+        incomeStatement.operatingIncome = value;
         break;
     }
   }
