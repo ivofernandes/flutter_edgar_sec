fac3bffe978ff5017830aa67d1c8597c6eafd160
Ivo Fernandes <ivomuze@gmail.com>
2024-01-17 12:59:50 +0000

    Fix extrapolation of the balance sheet

--- lib/src/model/financials/balance_mixins/balance_extrapolate.dart
+++ lib/src/model/financials/balance_mixins/balance_extrapolate.dart
@@ -8,35 +8,16 @@ mixin BalanceExtrapolate {
     BalanceSheet qb,
     BalanceSheet qc,
   ) {
-    balanceSheet.cashAndCashEquivalents = fullYear.cashAndCashEquivalents -
-        qc.cashAndCashEquivalents -
-        qb.cashAndCashEquivalents -
-        qa.cashAndCashEquivalents;
-    balanceSheet.shortTermInvestments =
-        fullYear.shortTermInvestments - qc.shortTermInvestments - qb.shortTermInvestments - qa.shortTermInvestments;
-
-    balanceSheet.accountsReceivable =
-        fullYear.accountsReceivable - qc.accountsReceivable - qb.accountsReceivable - qa.accountsReceivable;
-
-    balanceSheet.otherReceivables =
-        fullYear.otherReceivables - qc.otherReceivables - qb.otherReceivables - qa.otherReceivables;
-
-    balanceSheet.inventory = fullYear.inventory - qc.inventory - qb.inventory - qa.inventory;
-
-    balanceSheet.deferredTaxAssets =
-        fullYear.deferredTaxAssets - qc.deferredTaxAssets - qb.deferredTaxAssets - qa.deferredTaxAssets;
-
-    balanceSheet.otherCurrentAssets =
-        fullYear.otherCurrentAssets - qc.otherCurrentAssets - qb.otherCurrentAssets - qa.otherCurrentAssets;
-
-    balanceSheet.goodwill = fullYear.goodwill - qc.goodwill - qb.goodwill - qa.goodwill;
-
-    balanceSheet.totalLiabilities =
-        fullYear.totalLiabilities - qc.totalLiabilities - qb.totalLiabilities - qa.totalLiabilities;
-
-    balanceSheet.currentLiabilities =
-        fullYear.currentLiabilities - qc.currentLiabilities - qb.currentLiabilities - qa.currentLiabilities;
-
-    balanceSheet.equity = fullYear.equity - qc.equity - qb.equity - qa.equity;
+    balanceSheet.cashAndCashEquivalents = fullYear.cashAndCashEquivalents;
+    balanceSheet.shortTermInvestments = fullYear.shortTermInvestments;
+    balanceSheet.accountsReceivable = fullYear.accountsReceivable;
+    balanceSheet.otherReceivables = fullYear.otherReceivables;
+    balanceSheet.inventory = fullYear.inventory;
+    balanceSheet.deferredTaxAssets = fullYear.deferredTaxAssets;
+    balanceSheet.otherCurrentAssets = fullYear.otherCurrentAssets;
+    balanceSheet.goodwill = fullYear.goodwill;
+    balanceSheet.totalLiabilities = fullYear.totalLiabilities;
+    balanceSheet.currentLiabilities = fullYear.currentLiabilities;
+    balanceSheet.equity = fullYear.equity;
   }
 }
